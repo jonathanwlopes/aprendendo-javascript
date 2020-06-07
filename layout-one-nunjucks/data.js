41757ed32bbb85a657f2975b8e9014552e8bcc5c
Jonathan Lopes <jonathanwlopes93@gmail.com>
2020-06-07 09:28:35 -0300

    Javascript - HTML - CSS - Nunjucks

---
+++ layout-one-nunjucks/data.js
@@ -0,0 +1,54 @@
+module.exports = [
+  {
+    card: "card01",
+    id: "ZtMzB5CoekE",
+    title: "Aqui postamos nosso primeiro vídeo, com a data-base do nunjucks",
+    duration: "40 minutos",
+    price: "R$ 0,00 (Grátis)",
+    featured: true
+  },
+  
+  {
+    card: "card02",
+    id: "H43fXodv6WY",
+    title: "Aqui postamos nosso segundo vídeo, com a data-base do nunjucks",
+    duration: "40 minutos",
+    price: "R$ 0,00 (Grátis)",
+  },
+  
+  {
+    card: "card03",
+    id: "NYdrAIPLhIw",
+    title: "Aqui postamos nosso terceiro vídeo, com a data-base do nunjucks",
+    duration: "40 minutos",
+    price: "R$ 0,00 (Grátis)",
+    featured: true
+  },
+  
+
+  {
+    card: "card04",
+    id: "kn8zYcpIjO4",
+    title: "Aqui postamos nosso quarto vídeo, com a data-base do nunjucks",
+    duration: "40 minutos",
+    price: "R$ 0,00 (Grátis)",
+    featured: true
+  },
+  
+  {
+    card: "card05",
+    id: "12GFiyMuVmc",
+    title: "Aqui postamos nosso quinto vídeo, com a data-base do nunjucks",
+    duration: "40 minutos",
+    price: "R$ 0,00 (Grátis)",
+  },
+  
+  {
+    card: "card06",
+    id: "4OLCrClb_So",
+    title: "Aqui postamos nosso sexto vídeo, com a data-base do nunjucks",
+    duration: "40 minutos",
+    price: "R$ 0,00 (Grátis)",
+  }
+  
+]
