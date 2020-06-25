9b6e6fbb97abe2b491a760ac00c6aba734d22fce
Jonathan Lopes <jonathanwlopes93@gmail.com>
2020-06-24 22:04:08 -0300

    Atualização

--- primeiro-layout-tarefas/js/index.js
+++
@@ -1,7 +0,0 @@
-const $botaoEntrar = document.querySelector('#botaoEntrar')
-const $formSecaoLogin = document.querySelector('.formSecaoLogin')
-
-// $formSecaoLogin.addEventListener('click', function(evento){
-//   evento.preventDefault()
-
-// })
