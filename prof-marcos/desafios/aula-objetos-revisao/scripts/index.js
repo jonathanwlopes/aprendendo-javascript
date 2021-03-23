7ca9dcf08e14485e5aa6845e1aab49f059dbf995
Jonathan Lopes <jonathanwlopes93@gmail.com>
2021-03-23 20:11:27 -0300

    atualizacao-revisao-objetos

--- prof-marcos/desafios/aula-objetos-revisao/scripts/index.js
+++ prof-marcos/desafios/aula-objetos-revisao/scripts/index.js
@@ -5,19 +5,55 @@ const $panelOurTeam = document.querySelector(".panel-our-team");
 const createLayout = (profile) => {
   const wrapperProfile = document.createElement("div");
   wrapperProfile.classList.add("wrapper-profile");
+
   const imageProfile = document.createElement("img");
   imageProfile.classList.add("image-profile");
   imageProfile.src = profile.image;
-  const wrapperInfoProfile = document.createElement('div')
-  wrapperInfoProfile.classList.add('wrapper-info-profile')
-  const nameProfile = document.createElement('span')
-  nameProfile.classList.add('name-profile')
-  nameProfile.textContent = profile.nameProfile
+
+  const wrapperInfoProfile = document.createElement("div");
+  wrapperInfoProfile.classList.add("wrapper-info-profile");
+
+  const nameProfile = document.createElement("span");
+  nameProfile.classList.add("name-profile");
+  nameProfile.textContent = profile.nameProfile;
+
+  const carrerProfile = document.createElement("div");
+  carrerProfile.classList.add("carrer-profile");
+  carrerProfile.textContent = profile.carrerProfile;
+
+  const wrapperIcons = document.createElement("div");
+  wrapperIcons.classList.add("wrapper-icons");
+
+  const linkFacebook = document.createElement("a");
+  linkFacebook.href = profile.facebook;
+  linkFacebook.target = "_blank";
+  linkFacebook.classList.add("fab", "fa-facebook", "fa-lg", "_blank");
+
+  const linkTwitter = document.createElement("a");
+  linkTwitter.href = profile.twitter;
+  linkTwitter.target = "_blank";
+  linkTwitter.classList.add("fab", "fa-twitter", "fa-lg", "_blank");
+
+  const linkInstagram = document.createElement("a");
+  linkInstagram.href = profile.instagram;
+  linkInstagram.target = "_blank";
+  linkInstagram.classList.add("fab", "fa-instagram", "fa-lg", "_blank");
+
+  const linkGit = document.createElement("a");
+  linkGit.href = profile.github;
+  linkGit.target = "_blank";
+  linkGit.classList.add("fab", "fa-github", "fa-lg", "_blank");
 
   $panelOurTeam.appendChild(wrapperProfile);
   wrapperProfile.appendChild(imageProfile);
-  wrapperProfile.appendChild(wrapperInfoProfile)
-  wrapperInfoProfile.appendChild(nameProfile)
+  wrapperProfile.appendChild(wrapperInfoProfile);
+  wrapperInfoProfile.appendChild(nameProfile);
+  wrapperInfoProfile.appendChild(carrerProfile);
+  wrapperProfile.appendChild(wrapperIcons);
+  wrapperIcons.appendChild(linkFacebook);
+  wrapperIcons.appendChild(linkTwitter);
+  wrapperIcons.appendChild(linkInstagram);
+  wrapperIcons.appendChild(linkGit);
 };
 
 const printLayout = (listProfile) => {
--- prof-marcos/desafios/aula-objetos-revisao/scripts/mocks/index.js
+++ prof-marcos/desafios/aula-objetos-revisao/scripts/mocks/index.js
@@ -4,50 +4,50 @@ const listProfile = [
       "https://templates.envytheme.com/tracer/default/assets/img/scientist/img1.png",
     nameProfile: "Gaby Campbell",
     carrerProfile: "Data Management",
-    facebook: "www.google.com",
-    twitter: "www.google.com",
-    intagram: "www.google.com",
-    github: "www.google.com",
+    facebook: "http://facebook.com",
+    twitter: "http://twitter.com",
+    instagram: "http://instagram.com",
+    github: "http://github.com"
   },
   {
     image:
       "https://templates.envytheme.com/tracer/default/assets/img/scientist/img5.png",
     nameProfile: "Suely Gutierrez",
     carrerProfile: "Data Scientist",
-    facebook: "www.google.com",
-    twitter: "www.google.com",
-    intagram: "www.google.com",
-    github: "www.google.com",
+    facebook: "http://facebook.com",
+    twitter: "http://twitter.com",
+    instagram: "http://instagram.com",
+    github: "http://github.com"
   },
   {
     image:
       "https://templates.envytheme.com/tracer/default/assets/img/scientist/img6.png",
     nameProfile: "Marcos Miller",
     carrerProfile: "CEO",
-    facebook: "www.google.com",
-    twitter: "www.google.com",
-    intagram: "www.google.com",
-    github: "www.google.com",
+    facebook: "http://facebook.com",
+    twitter: "http://twitter.com",
+    instagram: "http://instagram.com",
+    github: "http://github.com"
   },
   {
     image:
       "https://templates.envytheme.com/tracer/default/assets/img/scientist/img7.png",
     nameProfile: "Lilian Cooper",
     carrerProfile: "Data Scientist",
-    facebook: "www.google.com",
-    twitter: "www.google.com",
-    intagram: "www.google.com",
-    github: "www.google.com",
+    facebook: "http://facebook.com",
+    twitter: "http://twitter.com",
+    instagram: "http://instagram.com",
+    github: "http://github.com"
   },
   {
     image:
       "https://templates.envytheme.com/tracer/default/assets/img/scientist/img8.png",
     nameProfile: "Jonathan Stivens",
     carrerProfile: "Analytical Solutions",
-    facebook: "www.google.com",
-    twitter: "www.google.com",
-    intagram: "www.google.com",
-    github: "www.google.com",
+    facebook: "http://facebook.com",
+    twitter: "http://twitter.com",
+    instagram: "http://instagram.com",
+    github: "http://github.com"
   },
 
   {
@@ -55,20 +55,20 @@ const listProfile = [
       "https://templates.envytheme.com/tracer/default/assets/img/scientist/img2.png",
     nameProfile: "Rafael Taylor",
     carrerProfile: "Data Scientist",
-    facebook: "www.google.com",
-    twitter: "www.google.com",
-    intagram: "www.google.com",
-    github: "www.google.com",
+    facebook: "http://facebook.com",
+    twitter: "http://twitter.com",
+    instagram: "http://instagram.com",
+    github: "http://github.com"
   },
   {
     image:
       "https://templates.envytheme.com/tracer/default/assets/img/scientist/img4.png",
     nameProfile: "Pedro Walker",
     carrerProfile: "Data Management",
-    facebook: "www.google.com",
-    twitter: "www.google.com",
-    intagram: "www.google.com",
-    github: "www.google.com",
+    facebook: "http://facebook.com",
+    twitter: "http://twitter.com",
+    instagram: "http://instagram.com",
+    github: "http://github.com"
   },
 ];
 export default listProfile;
