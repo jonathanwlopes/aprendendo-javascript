352a0b8a60bd3692372c1bf14c7de8628beb2b7b
Jonathan Lopes <jonathanwlopes93@gmail.com>
2021-03-23 14:29:35 -0300

    exercicio prof-marcos

--- prof-marcos/desafios/aula-objetos/scripts/index.js
+++ prof-marcos/desafios/aula-objetos/scripts/index.js
@@ -1,6 +1,8 @@
 import articleList from "./mocks/index.js";
 const $panelArticles = document.querySelector(".panel-articles");
 
+// Create Articles
+
 const createArticle = (article) => {
   const panelArticle = document.createElement("div");
   panelArticle.classList.add("panel-article");
@@ -10,17 +12,42 @@ const createArticle = (article) => {
   const articleTitle = document.createElement("h3");
   articleTitle.classList.add("article-title");
   articleTitle.textContent = article.title;
-  const articleContent = document.createElement('p')
-  articleContent.classList.add('article-content')
-  articleContent.textContent = article.content
-
+  const articleContent = document.createElement("p");
+  articleContent.classList.add("article-content");
+  articleContent.textContent = article.content;
 
   $panelArticles.appendChild(panelArticle);
   panelArticle.appendChild(image);
   panelArticle.appendChild(articleTitle);
-  panelArticle.appendChild(articleContent)
+  panelArticle.appendChild(articleContent);
 };
 
-for (const article of articleList) {
-  createArticle(article);
-}
+const printArticles = (articleList) => {
+  $panelArticles.innerHTML = "";
+  for (const article of articleList) {
+    createArticle(article);
+  }
+};
+
+printArticles(articleList);
+
+
+// Add new article
+const $addNewArticle = document.querySelector(".add-new-article");
+const $formImage = document.querySelector(".form-image");
+const $formTitle = document.querySelector(".form-title");
+const $formContent = document.querySelector(".form-content");
+
+$addNewArticle.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const addNewArticle = {
+    image: $formImage.value,
+    title: $formTitle.value,
+    content: $formContent.value,
+  };
+
+  articleList.push(addNewArticle);
+  printArticles(articleList);
+  console.log(articleList);
+});
+
