1969ddbae2c33550dbcfdaba0297417f1485ffd3
Jonathan Lopes <jonathanwlopes93@gmail.com>
2021-03-25 11:04:10 -0300

    update class objects

--- prof-marcos/desafios/aula-objetos-revisao01/scripts/index.js
+++ prof-marcos/desafios/aula-objetos-revisao01/scripts/index.js
@@ -1,32 +1,51 @@
 import listProduct from "./mocks/index.js";
 const $sectionSales = document.querySelector(".sales");
+const $btnSubmit = document.querySelector(".btn-submit");
+const $inputID = document.querySelector("#id-product");
+const $imageProduct = document.querySelector("#id-image");
+const $nameProduct = document.querySelector("#id-name")
+const $priceProduct = document.querySelector("#id-price")
+
+const insertProduct = (event) => {
+  
+};
+
+
+
+$btnSubmit.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const addNewProduct = {
+    idProduct: $inputID.value,
+    imageProduct: $imageProduct.value,
+    titleProduct: $nameProduct.value,
+    priceProduct: $priceProduct.value
+  };
+  console.log('entrei')
+});
 
 const createStructure = () => {
   const wrapperTitle = document.createElement("div");
   wrapperTitle.classList.add("wrapper-title-sales");
   $sectionSales.appendChild(wrapperTitle);
 
-  const arrivalsTitle = document.createElement('span')
-  arrivalsTitle.classList.add('newarrivals-title')
-  arrivalsTitle.textContent = 'NEW ARRIVALS'
-  wrapperTitle.appendChild(arrivalsTitle)
-  
-  const arrivalsLine = document.createElement('hr')
-  arrivalsLine.classList.add('newarrivals-line')
-  wrapperTitle.appendChild(arrivalsLine)
+  const arrivalsTitle = document.createElement("span");
+  arrivalsTitle.classList.add("newarrivals-title");
+  arrivalsTitle.textContent = "NEW ARRIVALS";
+  wrapperTitle.appendChild(arrivalsTitle);
+
+  const arrivalsLine = document.createElement("hr");
+  arrivalsLine.classList.add("newarrivals-line");
+  wrapperTitle.appendChild(arrivalsLine);
 
   const wrapperPanelArraivals = document.createElement("div");
   wrapperPanelArraivals.classList.add("wrapper-panel-newarrivals");
   $sectionSales.appendChild(wrapperPanelArraivals);
 
-  printProduct(wrapperPanelArraivals);
-
-  // const insertProduct = document.createElement('div')
-  // insertProduct.classList.add('insert-new-product')
-
+  getProduct(wrapperPanelArraivals);
 };
 
-const createProduct = (product, classSuper) => {
+const printProduct = (product, classSuper) => {
   const wrapperContentArrivals = document.createElement("div");
   wrapperContentArrivals.classList.add("wrapper-content-newarrivals");
   classSuper.appendChild(wrapperContentArrivals);
@@ -36,44 +55,43 @@ const createProduct = (product, classSuper) => {
   imageArrivals.src = product.imageProduct;
   wrapperContentArrivals.appendChild(imageArrivals);
 
-  const btnQuick = document.createElement('button')
-  btnQuick.classList.add('btn-quick')
-  btnQuick.textContent = "QUICK VIEW"
-  wrapperContentArrivals.appendChild(btnQuick)
+  const btnQuick = document.createElement("button");
+  btnQuick.classList.add("btn-quick");
+  btnQuick.textContent = "QUICK VIEW";
+  wrapperContentArrivals.appendChild(btnQuick);
 
-  const wrapperTitle = document.createElement('div')
-  wrapperTitle.classList.add('wrapper-title-newarrivals')
-  wrapperContentArrivals.appendChild(wrapperTitle)
+  const wrapperTitle = document.createElement("div");
+  wrapperTitle.classList.add("wrapper-title-newarrivals");
+  wrapperContentArrivals.appendChild(wrapperTitle);
 
-  const nameProduct = document.createElement('span')
-  nameProduct.classList.add('name-product')
-  nameProduct.textContent = product.titleProduct
-  wrapperTitle.appendChild(nameProduct)
+  const nameProduct = document.createElement("span");
+  nameProduct.classList.add("name-product");
+  nameProduct.textContent = product.titleProduct;
+  wrapperTitle.appendChild(nameProduct);
 
-  const priceProduct = document.createElement('span')
-  priceProduct.classList.add('price-product')
-  priceProduct.textContent = product.priceProduct
-  wrapperTitle.appendChild(priceProduct)
+  const priceProduct = document.createElement("span");
+  priceProduct.classList.add("price-product");
+  priceProduct.textContent = product.priceProduct;
+  wrapperTitle.appendChild(priceProduct);
 
-  const wrapperIcon = document.createElement('div')
-  wrapperIcon.classList.add('wrapper-icon-newarrivals')
-  wrapperContentArrivals.appendChild(wrapperIcon)
+  const wrapperIcon = document.createElement("div");
+  wrapperIcon.classList.add("wrapper-icon-newarrivals");
+  wrapperContentArrivals.appendChild(wrapperIcon);
 
-  const iconBag = document.createElement('a')
-  iconBag.classList.add('fas', 'fa-shopping-cart', 'fa-lg')
-  iconBag.href = '#'
-  wrapperIcon.appendChild(iconBag)
+  const iconBag = document.createElement("a");
+  iconBag.classList.add("fas", "fa-shopping-cart", "fa-lg");
+  iconBag.href = "#";
+  wrapperIcon.appendChild(iconBag);
 
-  const iconFavorite = document.createElement('a')
-  iconFavorite.classList.add('fas', 'fa-heart', 'fa-lg')
-  iconFavorite.href = '#'
-  wrapperIcon.appendChild(iconFavorite)
-
+  const iconFavorite = document.createElement("a");
+  iconFavorite.classList.add("fas", "fa-heart", "fa-lg");
+  iconFavorite.href = "#";
+  wrapperIcon.appendChild(iconFavorite);
 };
 
-const printProduct = (classSuper) => {
+const getProduct = (classSuper) => {
   for (const product of listProduct) {
-    createProduct(product, classSuper);
+    printProduct(product, classSuper);
   }
 };
 
--- prof-marcos/desafios/aula-objetos-revisao01/scripts/mocks/index.js
+++ prof-marcos/desafios/aula-objetos-revisao01/scripts/mocks/index.js
@@ -1,35 +1,42 @@
 const listProduct = [
   {
+    idProduct: '0',
     imageProduct: "http://magento2.magentech.com/themes/sm_market/pub/media/catalog/product/cache/5c64d10977f3f4e0b4e66d6fce87ba84/6/_/6.jpg",
     titleProduct: 'Grils Shoes Grils Sneaks',
     priceProduct: 'R$ 50,00'
   },
   {
+    idProduct: '1',
     imageProduct: "http://magento2.magentech.com/themes/sm_market/pub/media/catalog/product/cache/5c64d10977f3f4e0b4e66d6fce87ba84/5/_/5.jpg",
     titleProduct: 'Flex Racer 360 580 Red',
     priceProduct: 'R$ 85,00'
   },
   {
+    idProduct: '2',
     imageProduct: "http://magento2.magentech.com/themes/sm_market/pub/media/catalog/product/cache/5c64d10977f3f4e0b4e66d6fce87ba84/4/_/4.jpg",
     titleProduct: 'Latest Nation Valentine',
     priceProduct: 'R$ 125,00'
   },
   {
+    idProduct: '3',
     imageProduct: "http://magento2.magentech.com/themes/sm_market/pub/media/catalog/product/cache/5c64d10977f3f4e0b4e66d6fce87ba84/2/_/2.jpg",
     titleProduct: 'Michel Kors Rogers',
     priceProduct: 'R$ 30,00'
   },
   {
+    idProduct: '4',
     imageProduct: "http://magento2.magentech.com/themes/sm_market/pub/media/catalog/product/cache/5c64d10977f3f4e0b4e66d6fce87ba84/5/_/5.jpg",
     titleProduct: 'Flex Racer 360 580 Red',
     priceProduct: 'R$ 85,00'
   },
   {
+    idProduct: '5',
     imageProduct: "http://magento2.magentech.com/themes/sm_market/pub/media/catalog/product/cache/5c64d10977f3f4e0b4e66d6fce87ba84/5/_/5.jpg",
     titleProduct: 'Flex Racer 360 580 Red',
     priceProduct: 'R$ 85,00'
   },
   {
+    idProduct: '6',
     imageProduct: "http://magento2.magentech.com/themes/sm_market/pub/media/catalog/product/cache/5c64d10977f3f4e0b4e66d6fce87ba84/5/_/5.jpg",
     titleProduct: 'Flex Racer 360 580 Red',
     priceProduct: 'R$ 85,00'
